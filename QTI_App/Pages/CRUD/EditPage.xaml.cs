10dd690ac2b0c313e6218023587d24508829fdaf
Colin-van-Steenhoven <93527036+Colin-van-Steenhoven@users.noreply.github.com>
2024-02-27 10:46:57 +0100

    EditPage aangemaakt en een link gelegd tussen HomePage en EditPage. als ik dubbelklik op een vraag in de listview van de vragen opend de EditPage

--- QTI_App/Pages/CRUD/EditPage.xaml.cs
+++ QTI_App/Pages/CRUD/EditPage.xaml.cs
@@ -1,4 +1,3 @@
-using E4_The_Big_Three.Data;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -6,9 +5,6 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using QTI_App.Controllers;
-using QTI_App.Pages;
-using QTI_App.Pages.CRUD;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -20,32 +16,16 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace QTI_App.Pages
+namespace QTI_App.Pages.CRUD
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class HomePage : Page
+    public sealed partial class EditPage : Page
     {
-        private NavigationService _navigationService;
-        public HomePage()
+        public EditPage()
         {
             this.InitializeComponent();
-
-            using var db = new AppDbContext();
-            var questions = db.questions.ToList();
-            questionsLv.ItemsSource = questions;
-        }
-
-
-        private void questionsLv_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
-        {
-            _navigationService.NavigateTo<EditPage>();
-        }
-
-        private void addNewQuestionB_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
